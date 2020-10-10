a404a8e4634119b6c9aa038a42a633eca0901f67
Victor Morozov <morozoff.96@mail.ru>
2020-10-10 04:29:07 +0300

    Updated onboarding screen

---
+++ CarScanner/CarScanner/Slide.swift
@@ -0,0 +1,16 @@
+//
+//  Slide.swift
+//  CarScanner
+//
+//  Created by Виктор Морозов on 10.10.2020.
+//  Copyright © 2020 Olga Mikhnenko. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView, UIScrollViewDelegate{
+
+    @IBOutlet weak var OnboardingIcon: UIImageView!
+    @IBOutlet weak var Description: UILabel!
+    @IBOutlet weak var FinishButton: UIButton!
+}
